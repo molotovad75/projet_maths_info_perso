ce63d9c3ce8bda0be55ed10dd1081226db8dda6c
molotovad75 <adrien.lach1@gmail.com>
2021-12-30 00:12:19 +0100

    Programmation de plusieurs méthodes

--- src/application/automate/Automate.java
+++ src/application/automate/Automate.java
@@ -1,15 +1,18 @@
 package application.automate;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Automate {
 	private static final int nb_lettre_alphabet=3;
 	private ArrayList<Etat> liste_etat;
 	private ArrayList<String> liste_mots_reconnus;
-	private ArrayList<Etat> etat_acceptant;
+	private ArrayList<Etat> etat_acceptant; //L'état acceptant est simplement l'état qui accepte la transition équipé d'un ou plusieurs symboles de l'alphabet
 	
-	private boolean deterministe,complet;
+	private boolean deterministe,complet,standard;
 	
 	//Il faut savoir comment représenter une table des transitions
 	//Constructeur de test
@@ -18,7 +21,7 @@ public class Automate {
 		this.complet=complet;
 		liste_etat=new ArrayList<Etat>();
 		liste_mots_reconnus=new ArrayList<String>();
-		
+		etat_acceptant=new ArrayList<Etat>();
 	}
 	
 	//constructeur par défaut d'un automate
@@ -27,7 +30,7 @@ public class Automate {
 		this.complet=false;
 		liste_etat=new ArrayList<Etat>();
 		liste_mots_reconnus=new ArrayList<String>();
-		
+		etat_acceptant=new ArrayList<Etat>();
 	}
 	
 	public static int getNb_lettre_alphabet() {
@@ -36,8 +39,13 @@ public class Automate {
 	
 	//1 ère partie
 	
-	public void lecture_automate(BufferedReader file) {
-		
+	public void lecture_automate(File file) {
+		try {
+			FileReader fr=new FileReader(file);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	//**********************************************************************************************
@@ -47,11 +55,12 @@ public class Automate {
 	public String [][] affichage_automate() {
 		String tab[][]=new String[liste_etat.size()][nb_lettre_alphabet];
 		for (int j = 0; j < nb_lettre_alphabet; j++) {
-			// i est inférieur à la taille de la liste d'états et t est inférieur aux nombres de transitions entrantes dans notre état acceptant pour un état à l'indice i.
-			for (int i = 0, i2=0; i < liste_etat.size() && i2< etat_acceptant.size() ; i++, i2++) {
-				for (int t=0; t<liste_etat.get(i).getTransition_entrantes().size(); t++) {
-					if(liste_etat.get(i).getTransition_entrantes().get(t).getSymbole_alphabet()==etat_acceptant.get(i2).getTransition_entrantes().get(t).getSymbole_alphabet() && liste_etat.get(i).getTransition_entrantes().get(t).getSymbole_alphabet()[j]==Transition.getSymbole_alphabet_static()[j]) {
-						tab[i][j]="Etat :"+i+"\n Lettre de l'alphabet : "+ Transition.getSymbole_alphabet_static()[j]+" Etat acceptant : "+ etat_acceptant.get(i2).getNumero();
+			
+			for (int i = 0, i2=0; i < liste_etat.size() && i2< etat_acceptant.size() ; i++, i2++) {// i est inférieur à la taille de la liste d'états et t est inférieur aux nombres de transitions entrantes dans notre état acceptant pour un état à l'indice i.
+				for (int t=0, t2=0; t<liste_etat.get(i).getTransition_sortantes().size() && t<etat_acceptant.get(i2).getTransition_entrantes().size(); t++, t2++) {
+					//Dans le if on vérifie que l'alphabet pour une transition qui part de un état (liste_etat) est égale à l'alphabet arrivant à l'état acceptant avec cette même transition
+					if(liste_etat.get(i).getTransition_sortantes().get(t).getSymbole_alphabet()==etat_acceptant.get(i2).getTransition_entrantes().get(t2).getSymbole_alphabet() && liste_etat.get(i).getTransition_sortantes().get(t).getSymbole_alphabet()[j]==Transition.getSymbole_alphabet_static()[j]) {
+						tab[i][j]="Etat :"+i+"\n Lettre de l'alphabet : "+ Transition.getSymbole_alphabet_static()[j]+" Etat acceptant : "+ etat_acceptant.get(i2).getNumero()+"\n";
 						//Trouver un moyen de faire apparaitre tous les états acceptants.
 					}
 				}
@@ -68,10 +77,54 @@ public class Automate {
 	//3 ème partie
 	
 	public boolean isDeterministe() {
+		//Au plus 1 état initial
+		boolean foutu = false;
 		
+		int compteur=0;
+		for (Etat etat : liste_etat) {
+			if(etat.isInitial()==true && compteur>0) {
+				this.deterministe=false;
+				compteur++;
+			}else if(etat.isInitial()==true && compteur==0 ) {
+				compteur++;
+			}else if(etat.isInitial()==false && liste_etat.get(liste_etat.size()-1).isInitial()==false && compteur<=1) {
+				//Analyser les transitions qui sortent de chaque état. Il faut au maximum 1 transition sortante avec une même lettre de l'alphabet
+				for (int i = 0, tran=0, j=0 ; i < liste_etat.size() && tran < liste_etat.get(i).getTransition_sortantes().size() && j<nb_lettre_alphabet; i++, tran++, j++) {
+					 //Pour chaque transition de chaque état on vérifie qu'il n'y a pas plus de 1 transition sortantes avec le même symbole
+						if (liste_etat.get(i).getTransition_sortantes().get(tran).getSymbole_alphabet()[j]==liste_etat.get(i).getTransition_sortantes().get(tran-1).getSymbole_alphabet()[j] && tran>0) {
+							this.deterministe=false;
+							foutu=true;
+							j=0;
+						}else if(liste_etat.get(i).getTransition_sortantes().get(tran).getSymbole_alphabet()[j]!=liste_etat.get(i).getTransition_sortantes().get(tran-1).getSymbole_alphabet()[j] && tran>0) {
+							
+							if(tran==liste_etat.get(i).getTransition_sortantes().size()-1 && foutu==false) {
+								this.deterministe=true;
+							}else if(tran!=liste_etat.get(i).getTransition_sortantes().size()-1) {
+								j=0;
+							}
+							
+						}//liste_etat.get(i).getTransition_sortantes().get(tran).getSymbole_alphabet()[j]==liste_etat.get(i).getTransition_sortantes().get(tran-1).getSymbole_alphabet()[j] && tran>0
+				}//Fin For
+			}//FIN ELSE IF QUAND au plus 1 état est initial parmis tout le reste qui ne le sont pas.
+		}//Fin for each
+		return this.deterministe;
 	}
 	
 	public boolean isComplet() {
+		for (Etat etat : liste_etat) {
+			for (int i = 0; i < etat.getTransition_sortantes().size(); i++) {
+				if(etat.getTransition_sortantes().get(i)==null) {
+					this.complet=false;
+				}else if(etat.getTransition_sortantes().get(i)!=null && i==etat.getTransition_sortantes().size()-1) {
+					this.complet=true;
+				}
+			}
+		}
+		return this.complet;
+	}
+	
+	public boolean standardisation() {
+		Etat i;
 		
 	}
 	
@@ -86,13 +139,25 @@ public class Automate {
 	
 	//5ème partie
 	
-	public Etat ajouter_etat(Etat e) {
+	public Etat ajouter_etat(Etat e, boolean acceptant) {
+		e.setAcceptant(acceptant);
 		for (int i = 0; i < liste_etat.size(); i++) {
 			if(!liste_etat.contains(liste_etat.get(i))) {
 				e.setNumero(i);
+				if(i==0) {
+					e.setInitial(true); //Par défaut l'état ayant le numéro 0 est l'état initial.
+				}
 			}
 		}
 		liste_etat.add(e);
+		if(acceptant==true) {
+			for (int i = 0; i < etat_acceptant.size(); i++) {
+				if(!etat_acceptant.contains(etat_acceptant.get(i)) ) {
+					e.setNumero(i);
+				}
+			}
+			etat_acceptant.add(e);
+		}
 		return e;
 	}
 	
--- src/application/automate/Etat.java
+++ src/application/automate/Etat.java
@@ -5,8 +5,9 @@ import java.util.ArrayList;
 public class Etat {
 	private boolean initial, terminal;
 	private int numero;
-	private ArrayList<Transition> transition_entrantes;
+	private ArrayList<Transition> transition_entrantes,transition_sortantes;
 	private ArrayList<Etat> predecceseur;
+	private boolean acceptant;
 	
 	public Etat(int numero, boolean initial, boolean terminal) {
 		this.setTerminal(terminal);
@@ -50,6 +51,22 @@ public class Etat {
 
 	public void setPredecceseur(ArrayList<Etat> predecceseur) {
 		this.predecceseur = predecceseur;
+	}
+
+	public ArrayList<Transition> getTransition_sortantes() {
+		return transition_sortantes;
+	}
+
+	public void setTransition_sortantes(ArrayList<Transition> transition_sortantes) {
+		this.transition_sortantes = transition_sortantes;
+	}
+
+	public boolean isAcceptant() {
+		return acceptant;
+	}
+
+	public void setAcceptant(boolean acceptant) {
+		this.acceptant = acceptant;
 	}
 	
 	
